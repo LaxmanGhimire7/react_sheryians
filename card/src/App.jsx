24caf03300666b4fc6425ecfc74175128d601e4f
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-12-25 22:51:46 +0545

    react props

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import Card from './components/Card.jsx'
 
 const App = () => {
+  const [count, setCount] = useState(10);
+
+  const add = ()=>{
+    setCount(count+1)
+  }
+
   return (
     <div>
-        <Card />
-        
+        {/* <Card /> */}
+
+<h1>{count}</h1>
+<button onClick={add}>+</button>
     </div>
   )
 }
---
+++ props/props/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div>
+      <Card  name="Lakxh" />
+    </div>
+  )
+}
+
+export default App
