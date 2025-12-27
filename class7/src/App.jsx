6abcd739a7e36fd2c27defc8da4af2df5fe41069
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-12-27 23:27:54 +0545

    made component and transferred data, functions as props

--- class7/src/App.jsx
+++ class7/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-// import Card from "./Components/Card";
+import Card from "./Components/Card";
 
 const App = () => {
   const [userName, setUserName] = useState("");
@@ -74,24 +74,7 @@ const App = () => {
         {/* <Card /> */}
         {allUser.map((elem, idx) => {
           return (
-            <div key={idx} className="bg-black m-10 p-7 w-100 rounded-2xl">
-      <div className="flex flex-col items-center justify-center">
-        <img
-          className="h-24 w-24 object-center object-cover rounded-full"
-          src="https://images.unsplash.com/photo-1766366467962-685c0c081b70?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-        <h1 className="text-white">{elem.userName}</h1>
-        <p className="text-white font-semibold text-sm mt-2">{elem.phone}</p>
-        <p className="text-white text-center font-semibold text-sm leading-tight mt-1.5">
-          {elem.des}
-        </p>
-
-        <button onClick={()=>{
-          handleDelete(idx)
-        }} className="text-white bg-red-600 py-2 mt-3 rounded cursor-pointer active:scale-95 px-5" >Remove</button>
-      </div>
-    </div>
+        <Card elem={elem} key={idx} handleDelete={handleDelete} />
           );
         })}
       </div>
